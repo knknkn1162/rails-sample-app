ab15efddea88af36b5a378aef3df747ff81f406d
knknkn1162 <knknkn1162@gmail.com>
2017-12-28 21:02:16 +0900

    add users & signup_path

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,6 +14,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
   # test "the truth" do
   #   assert true
