72d338289fee185f2fd49763f9766b88079ebd66
knknkn1162 <knknkn1162@gmail.com>
2017-12-28 19:23:55 +0900

    test full-title

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    # if root_path renders homepage
+    assert_template 'static_pages/home'
+
+    assert_select "header a[href=?]", root_path, count: 2
+    assert_select "header a[href=?]", help_path
+    assert_select "footer a[href=?]", about_path
+    assert_select "footer a[href=?]", contact_path
+
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
