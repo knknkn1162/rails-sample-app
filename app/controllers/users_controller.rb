4e9eceb34b76be1d33dbb86672b009384e338ecb
knknkn1162 <knknkn1162@gmail.com>
2017-12-29 18:26:24 +0900

    impl login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       # same as redirect_to user_url(@user)
       # redirect flash at once
       flash[:success] = "Welcome to the sample app!"
